18a31b8b44dfe09a385c872a629ab0829f75a06f
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2025-01-13 16:39:59 +0200

    Added more sounds

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    public AudioClip explosionSound;
     // Start is called before the first frame update
     void Start()
     {
+        SoundManager.instance.PlaySFX(explosionSound);
         Destroy(gameObject, 0.5f);
     }
 }
--- Assets/Scripts/ShooterProjectile.cs
+++ Assets/Scripts/ShooterProjectile.cs
@@ -5,8 +5,11 @@ public class ShooterProjectile : MonoBehaviour
     public float speed = 20f;
     public int damage = 10;
 
+    public AudioClip laserSound;
+
     void Start()
     {
+        SoundManager.instance.PlaySFX(laserSound); // Play laser sound
         Destroy(gameObject, 8); // Destroy projectile after `lifetime` seconds
     }
 
